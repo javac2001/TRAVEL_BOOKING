f58d0f304c7e92dcfe9ff6ce954e9b486eb75d42
Kaushik <kaushikroykaushikroyezio9163@gmail.com>
2024-04-24 12:14:11 +0530

    Add ERROR.ejs

--- app.js
+++ app.js
@@ -125,6 +125,7 @@ app.all("*", (req, res, next) => {
 
 app.use((err, req, res, next) => {
     let {statusCode = 500 , message = "Something Went Wrong"} = err;
-    res.status(statusCode).send(message);
+    res.status(statusCode).render("error.ejs",{message});
+    // res.status(statusCode).send(message);
 
 })
