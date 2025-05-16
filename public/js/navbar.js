7303ce38e897ce3e379097ba9e86a7255916cffa
Kaushik <kaushikroykaushikroyezio9163@gmail.com>
2025-05-17 03:14:13 +0530

    NAVBAR (PHASE=>2)

--- public/js/navbar.js
+++ public/js/navbar.js
@@ -1,3 +1,10 @@
+window.addEventListener('scroll', function() {
+  const scrollPosition = window.scrollY;
+  if (scrollPosition > 100) {
+    console.log('Scrolled more than 100px!');
+  }
+});
+
 document.addEventListener('DOMContentLoaded', function () {
   const navToggle = document.getElementById('navToggle');
   const navLinks = document.getElementById('navLinks');
