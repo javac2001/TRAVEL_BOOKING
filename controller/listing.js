5df4d08acb5503b4861b0a8c45f11a576bb2ba8c
Kaushik <kaushikroykaushikroyezio9163@gmail.com>
2025-06-13 23:55:52 +0530

    Modify Map (PHASE->3)

--- controller/listing.js
+++ controller/listing.js
@@ -31,12 +31,17 @@ module.exports.showRoutePath = async (req, res) => {
 
 // CREATE-path
 module.exports.createRoutePath = async (req, res) => {
+    let query = req.body.listing.location;
+    const response = await fetch(`https://api.maptiler.com/geocoding/${encodeURIComponent(query)}.json?key=${process.env.CLOUD_MAP_TOKEN}`);
+    const data = await response.json();
+    
     const listingData = req.body.listing;
     let url = req.file.path;
     let filename = req.file.filename;
     let newListing = new dataListingModules(listingData);
     newListing.owner = req.user._id
     newListing.image = { url, filename };
+    console.log("Coordinates:", data.features[0].geometry); // [lng, lat]
     await newListing.save();
     req.flash('success', 'Listing created')
     res.redirect("/stayfinder");
--- public/js/map.js
+++ public/js/map.js
@@ -2,6 +2,6 @@ maptilersdk.config.apiKey = cloudMapToken;
 const map = new maptilersdk.Map({
   container: 'map', // container's id or the HTML element in which the SDK will render the map
   style: maptilersdk.MapStyle.STREETS,
-  center: [88.3629, 22.5744], // starting position [lng, lat]
+  center: [77.2088, 28.6139], // starting position [lng, lat]
   zoom: 14 // starting zoom
 });
