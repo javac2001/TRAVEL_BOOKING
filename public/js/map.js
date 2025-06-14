6e28b85dba94c89e049a1b1df053f728dbe3b059
Kaushik <kaushikroykaushikroyezio9163@gmail.com>
2025-06-14 21:01:33 +0530

    Modify Map (PHASE->6)

--- public/js/map.js
+++ public/js/map.js
@@ -3,5 +3,17 @@ const map = new maptilersdk.Map({
   container: 'map', // container's id or the HTML element in which the SDK will render the map
   style: maptilersdk.MapStyle.STREETS,
   center: mapLocation.geometry.coordinates, // starting position [lng, lat]
-  zoom: 14 // starting zoom
+  zoom: 15 // starting zoom
 });
+
+const el = document.createElement('div');
+el.className = 'custom-marker';
+
+const marker = new maptilersdk.Marker()
+  .setLngLat(mapLocation.geometry.coordinates)
+  .setPopup(
+    new maptilersdk.Popup({ offset: 25 })
+      .setHTML("<strong>Kolkata</strong><br>City of Joy")
+  )
+  .addTo(map);
+
